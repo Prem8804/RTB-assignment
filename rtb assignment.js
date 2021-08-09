4bf729f18950959d80b9143d98a13aac83b9cf3d
Prem8804 <84003945+Prem8804@users.noreply.github.com>
2021-08-09 17:51:46 +0530

    Add files via upload

---
+++ rtb assignment.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    var x = document.getElementById("img1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("img2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
